3d8aa692326ff0b43282f1909383ce50113a62d0
victor liu <victoliu@avatar3d.site>
2011-03-29 17:00:25 +0800

    added quick sort

--- basic/sorting.cpp
+++ basic/sorting.cpp
@@ -3,6 +3,8 @@
 #include "data_structures.h"
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 
@@ -103,4 +105,183 @@ void heap_sort(vector<int> &in)
 	Max_Heap heap(in);
 	heap.sort();
 	in = heap.get_v();
+}
+
+//==== Quick sort
+int partition(vector<int> &in, int p, int r, bool if_random = true)
+{
+	int m = p;
+	if (if_random)
+		m = p + rand()%(r - p);	
+
+	//cout << "m: " << m << endl;
+	swap(in[m], in[r]);
+	int i = p;
+	for (int j = p; j < r; j++)
+	{
+		if (in[j] <= in[r])
+		{
+			swap(in[i], in[j]);
+			i++;
+		}
+	}
+	swap(in[i], in[r]);
+	return i;
+}
+
+void quick_sort_work(vector<int> &in, int p, int r)
+{
+	if (p < r)
+	{
+		int q = partition(in, p, r, true);
+		quick_sort_work(in, p, q - 1);
+		quick_sort_work(in, q + 1, r);
+	}
+}
+
+void quick_sort(vector<int> &in)
+{
+	srand(static_cast<unsigned> (time(0)));
+	quick_sort_work(in, 0, in.size() - 1);
+}
+
+//============
+//==== Testing
+//============
+
+enum Permutation {Ascend, Descend, Random};
+bool if_print = false;
+vector<int> proto_ascend;
+vector<int> proto_descend;
+vector<int> proto_random;
+
+
+void create_ascend_array(vector<int> &in, int n)
+{
+	for (int i = 0; i < n; i++)
+		in.push_back(i);
+
+	if (if_print) 
+	{
+		cout << "Unsorted ascend array:" << endl;
+		print_v(in);
+	}
+}
+
+void create_descend_array(vector<int> &in, int n)
+{
+	for (int i = 0; i < n; i++)
+		in.push_back(n - 1 - i);
+
+	if (if_print) 
+	{
+		cout << "Unsorted descend array:" << endl;
+		print_v(in);
+	}
+}
+
+void create_random_array(vector<int> &in, int n)
+{
+	srand(static_cast<unsigned> (time(0)));
+	for (int i = 0; i < n; i++)
+		in.push_back(rand()%n);
+
+	if (if_print) 
+	{
+		cout << "Unsorted Random array:" << endl;
+		print_v(in);
+	}
+}
+
+void create_proto_arrays()
+{
+	int n = 10;
+	while (1)
+	{
+		cout << "\nPlease input array size(N>0): ";
+		cin >> n;
+		if (cin.eof())
+			exit(0);
+		else if (cin.fail() || n < 0)
+		{
+			cerr << "Invalid value" << endl;
+			cin.clear();
+			cin.ignore(10000, '\n');
+		}
+		else
+			break;
+	}
+
+	string s;
+	if (n > 1000)
+		cout << "Too big N, will not display array elements!" << endl;
+	else
+	{
+		while (1)
+		{
+			cerr << "Show arrays?(y/n): ";
+			cin >> s;
+			if (cin.eof())
+				exit(0);
+			else if (cin.fail() || (s[0] != 'y' && s[0] != 'n'))
+			{
+				cerr << "Invalid value" << endl;
+				cin.clear();
+				cin.ignore(10000, '\n');
+			}
+			else
+			{
+				s[0] == 'y' ? if_print = true : if_print = false;
+				break;
+			}
+		}
+	}
+
+	create_ascend_array(proto_ascend, n);
+	create_descend_array(proto_descend, n);
+	create_random_array(proto_random, n);
+}
+
+void verify(vector<int> &in)
+{
+	for (vector<int>::iterator it = in.begin(); it != in.end() - 1; it++)
+	{
+		if (*it > *(it + 1))
+		{
+			for (int t = 0; t < 3; t++)
+				cout << "!!! Failed to verify !!!";
+			cout << endl;
+			return;
+		}
+	}
+	//cout << "Pass verification !!!" << endl;
+}
+
+void call_sorting(vector<int> &in, pFn f)
+{
+	long start = clock();	
+	f(in);
+	long end = clock();	
+	double duration = static_cast<double> (end - start)/CLOCKS_PER_SEC; 
+
+	verify(in);
+
+	if (if_print) 
+		print_v(in);
+	cout << "Time: " << duration << " seconds" << endl;
+}
+
+void test_sorting(pFn f)
+{
+	vector<int> in_ascend(proto_ascend);
+	cout << "Sorting ascend array, result:" << endl;
+	call_sorting(in_ascend, f);
+
+	vector<int> in_descend(proto_descend);
+	cout << "Sorting descend array, result:" << endl;
+	call_sorting(in_descend, f);
+
+	vector<int> in_random(proto_random);
+	cout << "Sorting random array, result:" << endl;
+	call_sorting(in_random, f);
 }
--- basic/sorting.h
+++ basic/sorting.h
@@ -4,10 +4,18 @@
 #include <vector>
 using namespace std;
 
+typedef void (*pFn)(vector<int>&);
+
+//== Sorting
 void insertion_sort(vector<int> &arr);
 void merge_sort(vector<int> &arr);
 void selection_sort(vector<int> &arr);
 void bubble_sort(vector<int> &arr);
 void heap_sort(vector<int> &arr);
+void quick_sort(vector<int> &arr);
+
+//== Testing
+void create_proto_arrays();
+void test_sorting(pFn f);
 
 #endif
