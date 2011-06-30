7a199b51dedf508d7df3ce94c860218dd359486c
victor liu <victoliu@avatar3d.site>
2011-06-30 20:57:19 +0800

    modified ms100 009

--- problems/ms100_009.cpp
+++ problems/ms100_009.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 bool CheckPostOrder(vector<int> &v, int first, int last) {
-  if (first < last) {
+  if (first < last || v.empty()) {
     int root = v[last];
     int right_first, left_last;
     int i = last - 1;
@@ -18,19 +18,23 @@ bool CheckPostOrder(vector<int> &v, int first, int last) {
     while (i >= 0 && v[i] < root) --i;
     if (i > -1) 
       return false;
-    else
-      return CheckPostOrder(v, right_first, last -1) &&
-             CheckPostOrder(v, first, left_last);
+    bool left = true;
+    if (left_last > first)
+      left = CheckPostOrder(v, first, left_last);
+    bool right = true;
+    if (right_first < last - 1) 
+      right = CheckPostOrder(v, right_first, last -1);
+    return left && right;
   } 
-  return true;
+  return false;
 }
 
 int main() {
   //int arr[] = {5,7,6,9,11,10,8}; //Yes
   //int arr[] = {5,13,6,9,11,10,8}; //No
   //int arr[] = {5,7,6,8}; //Yes
-  int arr[] = {16,8}; //Yes
-  //int arr[] = {7,4,6,5}; //No
+  //int arr[] = {16,8}; //Yes
+  int arr[] = {7,4,6,5}; //No
   vector<int> v(arr, arr + sizeof(arr)/sizeof(int));
 
   if (CheckPostOrder(v, 0, v.size() - 1)) {
