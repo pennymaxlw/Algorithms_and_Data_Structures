611b850436ea9629b0ce326cd074a412070a9983
pennymaxlw <pennymaxlw@gmail.com>
2011-07-11 22:30:12 +0800

    modified ms100New 012

--- problems/ms100New_012.cpp
+++ problems/ms100New_012.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 
 bool CheckContinuousArray(int arr[5]) {
-  int min = arr[0], max = arr[0];
+  int min = -1, max = -1;
   for (int i = 0; i < 5; ++i) {
     if (arr[i] == 0) continue;
     if (arr[i] > max) max = arr[i];
     if (arr[i] < min) min = arr[i];
   }
-  return (max - min <= 4);
+  if (min == -1 || max == -1)
+    return true;
+  else
+    return (max - min <= 4);
 }
 
 
 int main() {
-  int arr[5] = {10,7,5,0,6};
+  //int arr[5] = {10,7,5,0,6};
+  int arr[5] = {0,0,0,0,6};
   if (CheckContinuousArray(arr))
     cout << "YES";
   else
