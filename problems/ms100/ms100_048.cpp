2c1838ce96c7e2b7debd818defdbd613e362dd67
victor liu <victoliu@avatar3d.site>
2011-07-29 18:59:55 +0800

    modefied ms100 048: 3rd function, more clear

--- problems/ms100/ms100_048.cpp
+++ problems/ms100/ms100_048.cpp
@@ -62,6 +62,27 @@ bool BinarySearch2(vector<int> &v, int key) {
   return false;
 }
 
+bool BinarySearch3(vector<int> &v, int key) {
+  if (v.empty()) return false;
+  int l = 0, r = v.size() - 1, m = 0;
+  while (l <= r) {
+    //cout << "l " << l << " r " << r << endl;
+    m = l + (r - l) / 2;
+    if (v[m] == key) return true;
+    if (v[l] >= v[m]) {
+      if (v[l] >= key && key > v[m])
+        r = m - 1;
+      else
+        l = m + 1;
+    } else {
+      if (v[m] > key && key >= v[r])
+        l = m + 1;
+      else
+        r = m - 1;
+    }
+  }
+  return false;
+}
 
 int main() {
   int arr[] = {6,5,4,3,2,1,10,9,8,7};
@@ -70,7 +91,7 @@ int main() {
   vector<int> v(arr, arr + sizeof(arr) / sizeof(int));
   //vector<int> v;
   for (int i = 0; i < v.size(); ++i) {
-    if (!BinarySearch2(v, v[i])) {
+    if (!BinarySearch3(v, v[i])) {
       cout << "NOT found " << v[i] << endl;
       return 0;
     }
