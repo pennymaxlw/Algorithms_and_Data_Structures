d598817f717c05911cbe2aca9cb41f92ddf7adb7
victor liu <victoliu@avatar3d.site>
2011-08-02 18:51:56 +0800

    modified ms100 086: added better implementation

--- problems/ms100/ms100_086.cpp
+++ problems/ms100/ms100_086.cpp
@@ -20,6 +20,15 @@ BTNode* BuildBinTreeFromArray(int *arr, int len, int pos = 0) {
   return p;
 }
 
+BTNode* BuildBinTreeFromArray2(int *arr, int start, int end) {
+  if (!arr || start > end) return NULL;
+  int mid = start + (end - start) / 2;
+  BTNode *p = new BTNode(arr[mid]);
+  p->left = BuildBinTreeFromArray2(arr, start, mid - 1);
+  p->right = BuildBinTreeFromArray2(arr, mid + 1, end);
+  return p;
+}
+
 void TraversalBinTree(BTNode *head) {
   if (head) {
     if (head->left) TraversalBinTree(head->left);
@@ -31,7 +40,9 @@ void TraversalBinTree(BTNode *head) {
 
 int main() {
  int arr[] = {1,2,3,4,5,6,7,8,9};
- BTNode *head = BuildBinTreeFromArray(arr, sizeof(arr) / sizeof(int));
+ //BTNode *head = BuildBinTreeFromArray2(arr, sizeof(arr) / sizeof(int));
+ int pos = 0;
+ BTNode *head = BuildBinTreeFromArray2(arr, 0, sizeof(arr) / sizeof(int) - 1);
  TraversalBinTree(head);
  cout << endl;
 }
