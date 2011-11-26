d5e59407f608d054263164ab4179c6c5af82feba
pennymaxlw <pennymaxlw@gmail.com>
2011-11-26 15:38:49 +0800

    modified ms100 021

--- problems/ms100/ms100_021.cpp
+++ problems/ms100/ms100_021.cpp
@@ -6,16 +6,31 @@ using namespace std;
 
 list<int> ls;
 void FindSum(int sum, int n) {
-  if (sum < 1 || n < 1) return;
-  if (sum > n) {
+  if (sum < 0 || n < 0)
+    return;
+  else if (sum > 0) {
     ls.push_front(n);
     FindSum(sum - n, n - 1);
     ls.pop_front();
     FindSum(sum, n - 1);
   } else {
-    cout << sum << " ";
     copy(ls.begin(), ls.end(), ostream_iterator<int>(cout, " "));
     cout << endl;
+  }
+}
+
+void FindSum2(int sum, int n, int cur) {
+  if (sum < 0)
+    return;
+  else if (sum == 0) {
+    copy(ls.begin(), ls.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+  } else {
+    for (int i = cur; i <= n; i++) {
+      ls.push_back(i);
+      FindSum2(sum - i, n, i + 1);
+      ls.pop_back();
+    }
   }
 }
 
@@ -23,4 +38,6 @@ int main() {
   int n = 5;
   int m = 7;
   FindSum(m, n);
+  cout << "--------" << endl;
+  FindSum2(m, n, 1);
 }
